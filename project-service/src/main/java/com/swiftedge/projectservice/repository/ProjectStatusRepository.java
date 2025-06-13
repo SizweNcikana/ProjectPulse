9b859d3d834dd68cc384e8ce445b9998d89e3526
SizweNcikana <sizwe.ncikana@gmail.com>
2025-06-13 17:05:35 +0200

    Completed Project Status flow

--- project-service/src/main/java/com/swiftedge/projectservice/repository/ProjectStatusRepository.java
+++ project-service/src/main/java/com/swiftedge/projectservice/repository/ProjectStatusRepository.java
@@ -4,8 +4,13 @@ import com.swiftedge.projectservice.entity.ProjectStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProjectStatusRepository extends JpaRepository<ProjectStatus, Long> {
 
     boolean existsByStatus(String status);
+
+
+    Optional<ProjectStatus> findByStatus(String statusName);
 }
