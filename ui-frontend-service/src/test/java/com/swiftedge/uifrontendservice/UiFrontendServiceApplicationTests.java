bad9f9f51f6337c2e8ab16c95a1a8dfe5449c9d8
SizweNcikana <sizwe.ncikana@gmail.com>
2025-07-23 18:56:29 +0200

    Created structure for the UI Frontend service

---
+++ ui-frontend-service/src/test/java/com/swiftedge/uifrontendservice/UiFrontendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftedge.uifrontendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiFrontendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
