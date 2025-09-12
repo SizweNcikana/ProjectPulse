b6500e30e17804333eb4eb0e1957b652f93beccb
SizweNcikana <sizwe.ncikana@gmail.com>
2025-09-12 03:25:31 +0200

    Created this class to use for Project response data instead of using ProjectDTO as this was complicating and making things messy as The ProjectDTO is used for Editing and saving Projects

---
+++ dto-library/src/main/java/com/swiftedge/dtolibrary/dto/ProjectResponseDTO.java
@@ -0,0 +1,29 @@
+package com.swiftedge.dtolibrary.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
+public class ProjectResponseDTO {
+
+    private String activeMenu;
+    private String activePage;
+
+    private Long projectId;
+    private String projectName;
+    private LocalDate startDate;
+    private Integer duration;
+    private String description;
+
+    private StatusDTO currentStatus;
+    private List<StatusDTO> statuses;
+}
